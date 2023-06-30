b0ebefb51d8cf59ec77731fbd6367386b1af962c
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-06-30 19:43:53 +0300

    payment screen UI done

--- lib/Pages/payment_gateway.dart
+++ lib/Pages/payment_gateway.dart
@@ -3,31 +3,43 @@ import 'package:fitness/constants/constants.dart';
 import 'package:flutter/material.dart';
 
 class PaymentGatewayScreen extends StatefulWidget {
-  const PaymentGatewayScreen({super.key});
+  const PaymentGatewayScreen({Key? key});
 
   @override
   State<PaymentGatewayScreen> createState() => _PaymentGatewayScreenState();
 }
 
 class _PaymentGatewayScreenState extends State<PaymentGatewayScreen> {
+  List<String> containerContents = [
+    'CBE Birr',
+    'CBE Mobile Banking',
+    'Dashen Amole',
+  ]; // Example list of container contents
+  List<String> containerImages = [
+    'assets/images/cbe_birr.png',
+    'assets/images/cbe_mobile_banking.png',
+    'assets/images/dashen_amole.png',
+  ]; // Example list of container images
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
         body: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-            image: AssetImage('assets/images/Rectangle 50.png'),
-            fit: BoxFit.cover,
-          )),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/Rectangle 50.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Expanded(
+              const Expanded(
                 flex: 1,
                 child: Padding(
-                  padding: const EdgeInsets.all(8.0),
+                  padding: EdgeInsets.all(8.0),
                   child: backButton(),
                 ),
               ),
@@ -42,27 +54,35 @@ class _PaymentGatewayScreenState extends State<PaymentGatewayScreen> {
                 flex: 10,
                 child: Padding(
                   padding: const EdgeInsets.symmetric(
-                      vertical: 8.0, horizontal: 10.0),
+                    vertical: 8.0,
+                    horizontal: 10.0,
+                  ),
                   child: Container(
+                    padding: EdgeInsets.only(top: size.height * 0.01),
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
-                      color: Color.fromRGBO(217, 217, 217, 0.50),
+                      color: const Color.fromRGBO(217, 217, 217, 0.50),
                     ),
                     child: GridView.builder(
-                      itemCount: 5,
+                      itemCount: containerContents.length,
                       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                         crossAxisCount: 1,
                         childAspectRatio: MediaQuery.of(context).size.width /
                             (MediaQuery.of(context).size.height / 6),
                       ),
                       itemBuilder: (context, index) {
+                        String content = containerContents[index];
+                        String image =
+                            containerImages[index % containerImages.length];
                         return Padding(
                           padding: const EdgeInsets.symmetric(
-                              vertical: 10.0, horizontal: 12.0),
+                            vertical: 10.0,
+                            horizontal: 12.0,
+                          ),
                           child: Container(
                             decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(12.0),
-                              gradient: LinearGradient(
+                              gradient: const LinearGradient(
                                 begin: Alignment.topLeft,
                                 end: Alignment.bottomRight,
                                 colors: [
@@ -70,7 +90,7 @@ class _PaymentGatewayScreenState extends State<PaymentGatewayScreen> {
                                   Color.fromRGBO(244, 56, 0, 0),
                                   Color(0xFFF43800),
                                 ],
-                                stops: [0.0153, 0.9821, 0.1234],
+                                stops: [0.0153, 0.9821, 0.9994],
                               ),
                             ),
                             child: Row(
@@ -79,14 +99,16 @@ class _PaymentGatewayScreenState extends State<PaymentGatewayScreen> {
                               children: [
                                 Padding(
                                   padding: const EdgeInsets.symmetric(
-                                      horizontal: 8.0),
+                                    horizontal: 8.0,
+                                    vertical: 4.0,
+                                  ),
                                   child: Column(
                                     children: [
                                       Image.asset(
-                                        'assets/images/cbe_mobile_banking.png',
-                                        scale: 1.5,
+                                        image,
+                                        height: size.height * 0.09,
                                       ),
-                                      Text('CBE Mobile Banking')
+                                      Text(content),
                                     ],
                                   ),
                                 ),
@@ -95,28 +117,29 @@ class _PaymentGatewayScreenState extends State<PaymentGatewayScreen> {
                                   child: TextButton(
                                     style: ButtonStyle(
                                       foregroundColor:
-                                          MaterialStatePropertyAll<Color>(
+                                          MaterialStateProperty.all<Color>(
                                         Color.fromRGBO(0, 0, 0, 0.7),
                                       ),
                                       alignment: Alignment.center,
                                       minimumSize:
                                           MaterialStateProperty.all<Size>(
-                                        Size(70,
+                                        const Size(70,
                                             30), // Adjust the width and height as desired
                                       ),
-                                      shape: MaterialStatePropertyAll<
+                                      shape: MaterialStateProperty.all<
                                           RoundedRectangleBorder>(
                                         RoundedRectangleBorder(
                                           borderRadius:
                                               BorderRadius.circular(10.0),
                                         ),
                                       ),
-                                      backgroundColor: MaterialStatePropertyAll(
+                                      backgroundColor:
+                                          MaterialStateProperty.all<Color>(
                                         Color.fromRGBO(217, 217, 217, 0.5),
                                       ),
                                     ),
                                     onPressed: () {},
-                                    child: Text('Buy'),
+                                    child: const Text('Buy'),
                                   ),
                                 ),
                               ],
@@ -127,7 +150,7 @@ class _PaymentGatewayScreenState extends State<PaymentGatewayScreen> {
                     ),
                   ),
                 ),
-              )
+              ),
             ],
           ),
         ),
