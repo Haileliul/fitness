fa0c7ef07db35920f353ee768e39fe3c1e0c3840
Medi-unique <mediunique00@gmail.com>
2023-07-05 10:02:08 +0300

    hi

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark(),
 
       title: 'Flutter Example',
-      home: PaymentGatewayScreen(),
+      home: WeSlideTest(),
       // SignUpScreen(),
       //  LoginScreen(),
       // FinalReportPage(),
