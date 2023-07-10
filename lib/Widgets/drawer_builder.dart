b064d8b5a1fe84369950f0e38b4548d1c5f0a34b
dagmawi-y <dagmawi.inbox@gmail.com>
2023-07-10 18:11:59 +0300

    drawer update

--- lib/Widgets/drawer_builder.dart
+++ lib/Widgets/drawer_builder.dart
@@ -23,43 +23,117 @@ class DrawerBuilder extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: EdgeInsets.only(
+              padding: const EdgeInsets.only(
                 left: 10.0,
                 top: 5.0,
                 bottom: 5.0,
                 right: 30.0,
               ),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                padding: EdgeInsets.symmetric(
-                  horizontal: 10.0,
-                  vertical: 10.0,
-                ),
-                child: DefaultTextStyle(
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 16.0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10.0,
+                      vertical: 10.0,
+                    ),
+                    child: const DefaultTextStyle(
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16.0,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Name : Jane Smith'),
+                          SizedBox(height: 10.0),
+                          Divider(
+                            height: 2.0,
+                            thickness: 1.0,
+                            color: kDrawerDividerColor,
+                          ),
+                          SizedBox(height: 10.0),
+                          Text('Age: 23'),
+                          SizedBox(height: 10.0),
+                          Divider(
+                            height: 2.0,
+                            thickness: 1.0,
+                            color: kDrawerDividerColor,
+                          ),
+                          SizedBox(height: 10.0),
+                          Text('Sex: F'),
+                        ],
+                      ),
+                    ),
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Name : John Smith'),
-                      SizedBox(height: 10.0),
-                      Text('Age: 23'),
-                      SizedBox(height: 10.0),
-                      Text('Sex: M'),
-                    ],
+                  DrawerButton(
+                    icon: Icons.chat,
                   ),
-                ),
+                  DrawerButton(
+                    icon: Icons.calculate,
+                  ),
+                  DrawerButton(
+                    icon: Icons.music_note,
+                  ),
+                ],
               ),
             )
           ],
         ),
       ),
+    );
+  }
+}
+
+class DrawerButton extends StatelessWidget {
+  DrawerButton({required this.icon});
+
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        SizedBox(
+          height: 10.0,
+        ),
+        SizedBox(
+          height: size.height * 0.1,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            padding: EdgeInsets.symmetric(
+              horizontal: 10.0,
+              vertical: 10.0,
+            ),
+            child: DefaultTextStyle(
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+                fontSize: 16.0,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(
+                    icon,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 const Color kPrimaryColor = Color(0xFF302B2B);
 
 const Color kSecondaryColor = Color(0xFFE81818);
+
+const kDrawerDividerColor = Colors.black;
